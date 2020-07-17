8816ca94677a99ab113f1d0965dead8c69102383
lenovo <fern>
2020-07-17 17:07:00 +0800

    极光推送=

---
+++ app/src/main/java/com/example/wzxjguang/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.wzxjguang;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity  {
+    public static final String MESSAGE_RECEIVED_ACTION = "com.example.jpushdemo.MESSAGE_RECEIVED_ACTION";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_MESSAGE = "message";
+    public static final String KEY_EXTRAS = "extras";
+    public static boolean isForeground = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ app/src/main/java/com/example/wzxjguang/PushService.java
@@ -0,0 +1,8 @@
+package com.example.wzxjguang;
+
+
+import cn.jpush.android.service.JCommonService;
+
+public class PushService extends JCommonService {
+
+}
